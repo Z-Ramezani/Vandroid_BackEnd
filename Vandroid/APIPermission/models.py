adef9a7608d492534abece460f48e1035d1b5a3a
Zahra Ramezani <miss.ramazani@yahoo.com>
2023-01-01 17:15:29 +0330

    complete permissions

--- Vandroid/APIPermission/models.py
+++ Vandroid/APIPermission/models.py
@@ -6,4 +6,3 @@ class APIPermission(models.Model):
     name = models.CharField(max_length=50)
     status = models.CharField(max_length=17)
     info = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
