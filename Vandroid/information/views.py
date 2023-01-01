f6f2bc2ffd2617ef4873dacc40d085229ec74ae1
Zahra Ramezani <miss.ramazani@yahoo.com>
2023-01-01 11:33:23 +0330

    information URLs

--- Vandroid/information/views.py
+++ Vandroid/information/views.py
@@ -1,1 +1,16 @@
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from information.models import Information
+from information.serializers import InformationSerializer
+
+
+class ListInfo(APIView):
+    id = 0
+
+    def post(self, request):
+        inf = Information.objects.filter(
+            nameApp=request.data['nameApp']).values()
+        info = InformationSerializer(instance=inf, many=True)
+        ListInfo.id = ((info.data['id']))
+        return Response(data=info.data)
