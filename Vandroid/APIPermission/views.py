a19030c157b8e78de74c4c919ee7e339feef0fcc
Zahra Ramezani <miss.ramazani@yahoo.com>
2023-01-25 12:10:44 +0330

    edited

--- Vandroid/APIPermission/views.py
+++ Vandroid/APIPermission/views.py
@@ -3,11 +3,9 @@ from rest_framework.response import Response
 from APIPermission.models import APIPermission
 from APIPermission.serializers import APIPermissionSerializer
 from information import views
-from rest_framework.generics import GenericAPIView
 
-class ListAPIPermission(GenericAPIView):
+class ListAPIPermission(APIView):
     def get(self, request):
-        serializer_class=APIPermissionSerializer
         APIPer = APIPermission.objects.filter(target_id=views.ListInfo.id).values()
         APIPerm = APIPermissionSerializer(instance=APIPer, many=True)
         return Response(data=APIPerm.data)
--- Vandroid/information/views.py
+++ Vandroid/information/views.py
@@ -3,14 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from information.models import Information
 from information.serializers import InformationSerializer
-from rest_framework.generics import GenericAPIView
 
 
-class ListInfo(GenericAPIView):
+class ListInfo(APIView):
     id = 0
     data=""
     def post(self, request):
-
         info = Information.objects.filter(nameApp=request.data['nameApp']).values()
         info2 = InformationSerializer(instance=info, many=True)
         ListInfo.id = ((info2.data[0]['id']))
